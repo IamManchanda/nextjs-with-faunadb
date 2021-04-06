a2c3541a63235511594b79d8ba5cdd3070be7485
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-06 15:05:55 +0530

    Working Pass on get customers api route

--- src/pages/api/customers.js
+++ src/pages/api/customers.js
@@ -5,17 +5,17 @@ const q = faunadb.query;
 const client = new faunadb.Client({ secret });
 
 async function handleCustomers(req, res) {
-  switch (method) {
+  switch (req.method) {
     case "GET":
       try {
         const { data } = await client.query(
-          q.Map(
-            q.Paginate(q.Match(q.Index("all_customers"))),
-            (ref = q.Get(ref)),
+          q.Map(q.Paginate(q.Match(q.Index("all_customers"))), (ref) =>
+            q.Get(ref),
           ),
         );
         res.status(200).json(data);
       } catch (error) {
+        console.log({ error });
         res.status(500).json({
           message: "Something went Wrong!",
         });
