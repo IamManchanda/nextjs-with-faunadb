7f1fef93667c5e42becdcd34e34602d2c577b843
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-04-06 20:52:33 +0530

    More work in markup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import { Fragment, useState, useEffect } from "react";
-import { Heading } from "@chakra-ui/react";
+import { Heading, Flex, Stack, Box } from "@chakra-ui/react";
 
 function PageIndex() {
   const [data, setData] = useState([]);
@@ -21,13 +21,29 @@ function PageIndex() {
         <title>Next.js with FaunaDB</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Heading as="h1" my={2} textAlign="center">
-        Next.js, FaunaDB & Serverless
-      </Heading>
 
-      <Heading as="h3" my={2} textAlign="center">
-        Customer Data
-      </Heading>
+      <Box>
+        <Heading as="h1" my={2} textAlign="center">
+          Next.js, FaunaDB & Serverless
+        </Heading>
+
+        <Heading as="h3" my={2} textAlign="center">
+          Customer Data
+        </Heading>
+      </Box>
+      <Flex mt={12} align="center" justify="center">
+        <Stack>
+          <Heading mb={6} as="h4">
+            Name:
+          </Heading>
+          <Heading mb={6} as="h4">
+            Phone Number:
+          </Heading>
+          <Heading mb={6} as="h4">
+            Credit Card:
+          </Heading>
+        </Stack>
+      </Flex>
     </Fragment>
   );
 }
